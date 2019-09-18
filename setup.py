32d14268288cf9623310111fb41ea24bfba39819
Aroon Chande <git@atc.io>
2019-09-18 15:10:35 -0400

    Python styling changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tagore",
-    version="1.0.post1",
+    version="1.0.post2",
     author="Applied Bioinformatics Laboratory",
     author_email="abil@ihrc.com",
     description="A simple way to visualize features on human chromosome ideograms as shown in this article",
