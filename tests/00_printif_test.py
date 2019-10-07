430e0b0fcd2e1216040fe9e474dc497d386da64e
Aroon Chande <git@atc.io>
2019-10-07 09:46:32 -0400

    start adding tests for tagore, low priotiy

---
+++ tests/00_printif_test.py
@@ -0,0 +1,20 @@
+#!/bin/env python3
+import pytest
+import sys
+from os import getcwd, path, pardir
+sys.path.append(path.abspath(path.join(getcwd())))
+from tagore import printif
+
+def test_printif_true():
+    from io import StringIO
+    output = StringIO()
+    sys.stdout = output
+    printif("Test passed", True)
+    assert output.getvalue().strip()  == "Test passed"
+
+def test_printif_false():
+    from io import StringIO
+    output = StringIO()
+    sys.stdout = output
+    printif("Test false", False)
+    assert output.getvalue().strip() == ''
